1a0f280b62a15e7be93fbc6965cebf2554a81a1d
Nguyễn Xuân Bách <110024359+Yukimarin@users.noreply.github.com>
2022-07-28 21:31:47 +0700

    Homework1

---
+++ app.js
@@ -0,0 +1,26 @@
+// Bài 1 
+// Viết chương trình nhập điểm của một sinh viên cho các môn: Vật lý, Hóa học, và Sinh học. 
+// Sau đó hiển thị điểm trung bình và tổng của những điểm này.
+// let A = Number(prompt ("Nhập điểm môn Vật lý"));
+// let B = Number(prompt ("Nhập điểm môn Hóa học"));
+// let C = Number(prompt ("Nhập điểm môn Sinh học"));
+// let tong = A + B + C;
+// let trungbinh = ((A + B + C)/3);
+// console.log("Điểm trung bình là: " + trungbinh);
+// console.log("Điểm tổng là: " + tong);
+
+// Bài 2
+// Viết chương trình nhập một giá trị là độ 0C (Celsius) và chuyển nó sang độ 0F (Fahrenheit). [Hướng dẫn: C/5 = (F-32)/9]
+// let cTemp =prompt('Enter the temperature in Celcius');
+// let convert = Number((9*cTemp+(32*5))/5);
+// console.log(`${cTemp} (C) = ${convert} (F)`);
+
+// Bài 3: Viết chương trình tính diện tích hình tròn
+// let bankinh =prompt('Nhập độ dài bán kính');
+// let dientich = Number(bankinh)*Number(bankinh)*Math.PI;
+// console.log(`Diện tích hình tròn bằng ${dientich}`);
+
+// Bài 4: Viết chương trình tính chu vi hình tròn
+// let bankinh =prompt('Nhập độ dài bán kính');
+// let chuvi = Number(bankinh)*2*Math.PI;
+// console.log(`Chu vi hình tròn bằng ${chuvi}`);
